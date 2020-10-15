72c83610f69236784681b8f5e158053802dd44aa
huanghuiy288 <2450516728@qq.com>
2020-10-15 13:13:59 +0800

    模型基础建设

---
+++ JiajuVRproject/Assets/Scrips/JiajuMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//using UnityEditor.Sprites ;
+public class JiajuMenu : MonoBehaviour
+{
+    //public GameObject Gmenue;
+    public GameObject btnObj;
+    public GameObject caidan;
+    public Sprite expan;
+    public Sprite back;
+    Button btn;
+    bool isshow = false;
+    // Use this for initialization
+    void Start()
+    {
+        caidan.SetActive(isshow);
+        btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            isshow = !isshow;
+            caidan.SetActive(isshow);
+            if (isshow)
+            {
+                btn.GetComponent<Image>().sprite = expan;
+            }
+            else
+            {
+                btn.GetComponent<Image>().sprite = back;
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ JiajuVRproject/Assets/Scrips/Jiajuchance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jiajuchance : MonoBehaviour
+{
+    public GameObject btnObj;
+    public GameObject objSet;
+    public GameObject objBuy;
+    bool isshow = false;
+    Button btn;
+    public Sprite expan;
+    public Sprite back;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objSet.SetActive(isshow);
+        objBuy.SetActive(isshow);
+        btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate ()
+        {
+            isshow = !isshow;
+            objSet.SetActive(isshow);
+            objBuy.SetActive(isshow);
+            if (isshow)
+            {
+                btn.GetComponent<Image>().sprite = expan;
+            }
+            else
+            {
+                btn.GetComponent<Image>().sprite = back;
+            }
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
